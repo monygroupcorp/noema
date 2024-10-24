b8d1ab869c2ccb15115daaf572020ae3af931656
lifehaverdev <jfkwebdev@gmail.com>
2024-10-23 22:23:02 -0500

    loser in make.js

--- commands/make.js
+++ commands/make.js
@@ -372,6 +372,22 @@ function prepareRequest(promptObj) {
             }
             //console.log('body for mog',body)
             break;
+        case 'LOSER':
+        console.log('oh loser alright')
+        console.log(comfydeployid)
+        body = {
+            deployment_id: comfydeployid,
+            webhook: webHook,
+            inputs: {
+                noise_seed: promptObj.seed,
+                cfg: promptObj.cfg,
+                input_height: promptObj.photoStats.height,
+                input_width: promptObj.photoStats.width,
+                input_text: `${promptObj.finalPrompt}`,
+            }
+        }
+        //console.log('body for mog',body)
+            break;
         case 'FLUX': 
             body = {
                 deployment_id: comfydeployid,
